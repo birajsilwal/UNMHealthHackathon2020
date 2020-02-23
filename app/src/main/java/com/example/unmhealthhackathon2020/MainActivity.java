88c8dbae1aac877ca9b069c331edd757f91b6a7f
biraj silwal <bsilwal@cs.unm.edu>
2020-02-22 18:06:23 -0700

    made some UI change created logic for increasing and decreasing number in textview when up/down arrow button is clicked

--- app/src/main/java/com/example/unmhealthhackathon2020/MainActivity.java
+++ app/src/main/java/com/example/unmhealthhackathon2020/MainActivity.java
@@ -1,117 +1,74 @@
 package com.example.unmhealthhackathon2020;
-import java.io.IOException;
-import java.util.Set;
-import java.util.UUID;
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener {
 
-    Button i1;
-    TextView t1;
+import androidx.appcompat.app.AppCompatActivity;
 
-    String address = null , name=null;
+public class MainActivity extends AppCompatActivity {
 
-    BluetoothAdapter myBluetooth = null;
-    BluetoothSocket btSocket = null;
-    Set<BluetoothDevice> pairedDevices;
-    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    Button elevatorButton;
+    TextView userNumber, setUserInput;
+    ImageView upArrow, downArrow;
+
+    int current = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {setw();} catch (Exception e) {}
-    }
 
-    @SuppressLint("ClickableViewAccessibility")
-    private void setw() throws IOException
-    {
-        t1=(TextView)findViewById(R.id.textView1);
-        bluetooth_connect_device();
+        elevatorButton = findViewById(R.id.elevatorButton);
+        upArrow = findViewById(R.id.upArrow);
+        downArrow = findViewById(R.id.downArrow);
+
+        userNumber = findViewById(R.id.userNumber);
 
 
+        elevatorButton = findViewById(R.id.elevatorButton);
 
-        i1=(Button)findViewById(R.id.button1);
+        setUserInput = findViewById(R.id.setUserInput);
 
-        i1.setOnTouchListener(new View.OnTouchListener()
-        {   @Override
-        public boolean onTouch(View v, MotionEvent event){
-            if(event.getAction() == MotionEvent.ACTION_DOWN) {led_on_off("f");}
-            if(event.getAction() == MotionEvent.ACTION_UP){led_on_off("b");}
-            return true;}
+        elevatorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, elevatorButton.class);
+
+                startActivity(intent);
+            }
         });
 
     }
 
-    private void bluetooth_connect_device() throws IOException
-    {
-        try
-        {
-            myBluetooth = BluetoothAdapter.getDefaultAdapter();
-            address = myBluetooth.getAddress();
-            pairedDevices = myBluetooth.getBondedDevices();
-            if (pairedDevices.size()>0)
-            {
-                for(BluetoothDevice bt : pairedDevices)
-                {
-                    address=bt.getAddress().toString();name = bt.getName().toString();
-                    Toast.makeText(getApplicationContext(),"Connected", Toast.LENGTH_SHORT).show();
-
-                }
-            }
-
-        }
-        catch(Exception we){}
-        myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
-        BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
-        btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
-        btSocket.connect();
-        try { t1.setText("BT Name: "+name+"\nBT Address: "+address); }
-        catch(Exception e){}
-    }
-
-    @Override
-    public void onClick(View v)
-    {
-        try
-        {
-
-        }
-        catch (Exception e)
-        {
-            Toast.makeText(getApplicationContext(),e.getMessage(), Toast.LENGTH_SHORT).show();
-
-        }
+    public void counterUp(View view) {
+        current++;
+        userNumber.setText(Integer.toString(current));
 
     }
 
-    private void led_on_off(String i)
-    {
-        try
-        {
-            if (btSocket!=null)
-            {
+    public void counterDown(View view) {
+        current--;
+        userNumber.setText(Integer.toString(current));
 
-                btSocket.getOutputStream().write(i.toString().getBytes());
-            }
+    }
 
+    public void setUserInput(View view) {
+
+        if (current == 1) {
+            setUserInput.setText("You're going to " + current + "st floor.");
         }
-        catch (Exception e)
-        {
-            Toast.makeText(getApplicationContext(),e.getMessage(), Toast.LENGTH_SHORT).show();
-
+        else if (current == 2) {
+            setUserInput.setText("You're going to " + current + "nd floor.");
+        }
+        else if (current == 3) {
+            setUserInput.setText("You're going to " + current + "rd floor.");
+        }
+        else {
+            setUserInput.setText("You're going to " + current + "th floor.");
         }
 
     }
--- app/src/main/java/com/example/unmhealthhackathon2020/elevatorButton.java
+++ app/src/main/java/com/example/unmhealthhackathon2020/elevatorButton.java
@@ -1,116 +1,112 @@
-//package com.example.unmhealthhackathon2020;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.annotation.SuppressLint;
-//import android.bluetooth.BluetoothAdapter;
-//import android.bluetooth.BluetoothDevice;
-//import android.bluetooth.BluetoothSocket;
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.util.Log;
-//import android.view.MotionEvent;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//import android.widget.Toast;
-//
-//import java.io.IOException;
-//import java.util.Set;
-//import java.util.UUID;
-//
-//public class elevatorButton extends AppCompatActivity {
-//
-//    ImageView backToMain;
-//
-//    public Button one;
-//    public TextView bluetoothNameAddress;
-//
-//    String address = null;
-//    String name = null;
-//
-//    BluetoothAdapter bluetoothAdapter = null;
-//    BluetoothSocket bluetoothSocket = null;
-//    Set<BluetoothDevice> pairedDevices;
-//    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_elevator_button);
-//
-//        backToMain = findViewById(R.id.ivBack);
-//
-//        backToMain.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(elevatorButton.this, MainActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        try {
-//            userActions();
-//        }
-//        catch (Exception e) {}
-//
-//    }
-//
-//    @SuppressLint("ClickableViewAccessibility")
-//    private void userActions() throws IOException {
-//        bluetoothConnectDevice();
-//        one = findViewById(R.id.one);
-//
-//        one.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-//                    ledOnOff("f");
-//                    Log.i("onebutton", "clicked one");
-//                }
-//                if (event.getAction() == MotionEvent.ACTION_UP) {ledOnOff("t");}
-//                return true;
-//            }
-//        });
-//    }
-//
-//    private void bluetoothConnectDevice() throws IOException {
-//        try {
-//            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-//            address = bluetoothAdapter.getAddress();
-//            pairedDevices = bluetoothAdapter.getBondedDevices();
-//
-//            if (pairedDevices.size() >= 0) {
-//                for (BluetoothDevice bluetoothDevice : pairedDevices) {
-//                    address = bluetoothDevice.getAddress().toString();
-//                    name = bluetoothDevice.getName().toString();
-//                    Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        }
-//        catch (Exception exception) {}
-//        // get the mobile bluetooth device
-//        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-//        // connect to the device's address and checks if it's available
-//        BluetoothDevice dispositivo = bluetoothAdapter.getRemoteDevice(address);
-//        // create a RFCOMM (SPP) connection
-//        bluetoothSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);
-//        bluetoothSocket.connect();
-//
-//        try {
-//            bluetoothNameAddress.setText("Name: " + name + "\naddress: " + address);
-//        }
-//        catch (Exception exception){}
-//    }
-//
-//    private void ledOnOff(String i) {
-//        try {
-//            if (bluetoothSocket != null) {
-//                bluetoothSocket.getOutputStream().write(i.getBytes());
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+package com.example.unmhealthhackathon2020;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class elevatorButton extends Activity implements OnClickListener {
+
+    Button i1;
+    ImageView ivBack;
+    TextView t1;
+
+    String address = null , name=null;
+
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    Set<BluetoothDevice> pairedDevices;
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elevator_button);
+        ivBack = findViewById(R.id.ivBack);
+
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(elevatorButton.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        try {setw();} catch (Exception e) {}
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setw() throws IOException {
+        t1=(TextView)findViewById(R.id.textView1);
+        bluetooth_connect_device();
+
+        i1=(Button)findViewById(R.id.button1);
+
+        i1.setOnTouchListener(new View.OnTouchListener()
+        {   @Override
+        public boolean onTouch(View v, MotionEvent event){
+            if(event.getAction() == MotionEvent.ACTION_DOWN) {led_on_off("f");}
+            if(event.getAction() == MotionEvent.ACTION_UP){led_on_off("b");}
+            return true;}
+        });
+    }
+
+    private void bluetooth_connect_device() throws IOException {
+        try {
+            myBluetooth = BluetoothAdapter.getDefaultAdapter();
+            address = myBluetooth.getAddress();
+            pairedDevices = myBluetooth.getBondedDevices();
+            if (pairedDevices.size()>0) {
+                for(BluetoothDevice bt : pairedDevices) {
+                    address=bt.getAddress().toString();name = bt.getName().toString();
+                    Toast.makeText(getApplicationContext(),"Connected", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        }
+        catch(Exception we){}
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
+        BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
+        btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
+        btSocket.connect();
+        try { t1.setText("BT Name: "+name+"\nBT Address: "+address); }
+        catch(Exception e){}
+    }
+
+    @Override
+    public void onClick(View v) {
+        try
+        {
+
+        }
+        catch (Exception e) {
+            Toast.makeText(getApplicationContext(),e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void led_on_off(String i) {
+        try {
+            if (btSocket!=null) {
+                btSocket.getOutputStream().write(i.toString().getBytes());
+            }
+        }
+        catch (Exception e) {
+            Toast.makeText(getApplicationContext(),e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
